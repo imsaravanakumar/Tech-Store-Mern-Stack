bc36042ffb448b18af564c46e03005d1092a1c9a
Saravana Kumar <144697260+imsaravanakumar@users.noreply.github.com>
2025-08-21 20:31:55 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Layout/Hero';
 import NewArrivals from '../pages/NewArrivals';
 import AboutUs from '../components/Comman/Aboutus';
-import ContactUs from '../components/Comman/Contactus';
+import ContactUs from '../components/Comman/ContactUs';
 
 const Home = () => {
   return (
